032df976fd97ef06fdb9bec50438e87d0657b758
Novarty <artem.ivanov9616@gmail.com>
2017-11-17 17:20:19 +0300

    Fixed small issues. Added gem responders to the project.

--- app/controllers/products_controller.rb
+++ app/controllers/products_controller.rb
@@ -1,12 +1,10 @@
 class ProductsController < ApplicationController
   # before_action :authenticate_user!, except: [:index, :show]
   before_action :authenticate_manager!, except: [:index, :show]
-  before_action :set_product, only: [:show, :edit, :update, :destroy]
 
   expose :products, ->{ Product.all }
   expose :product
 
-  # GET /products
   def index; end
 
   def show; end
@@ -16,36 +14,23 @@ class ProductsController < ApplicationController
   def edit; end
 
   def create
-    product = current_manager.products.new(product_params)
-    if product.save
-      redirect_to product_path(product), notice: 'The product was successfully created.'
-    else
-      render :new
-    end
+    product.manager = current_manager
+    product.save
+    respond_with product
   end
 
-  # PATCH/PUT /products/1
   def update
-    if @product.update(product_params)
-      redirect_to @product, notice: 'The product was successfully updated.'
-    else
-      render :edit
-    end
+    product.update(product_params)
+    respond_with product
   end
 
-  # DELETE /products/1
   def destroy
-    @product.destroy
-      redirect_to products_url, notice: 'The product was successfully erased.'
+    product.destroy
+    respond_with product
   end
 
   private
-    # Use callbacks to share common setup or constraints between actions.
-    def set_product
-      @product = Product.find(params[:id])
-    end
 
-    # Never trust parameters from the scary internet, only allow the white list through.
     def product_params
       params.require(:product).permit(:name, :amount)
     end
