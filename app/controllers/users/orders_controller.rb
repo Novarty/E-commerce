f8984966feec0a02754a424a0f93eeafb19db158
Novarty <artem.ivanov9616@gmail.com>
2017-12-11 15:54:02 +0300

    Updated orders.

--- app/controllers/users/orders_controller.rb
+++ app/controllers/users/orders_controller.rb
@@ -1,24 +1,34 @@
 class Users::OrdersController < ApplicationController
   before_action :authenticate_user!
 
-  def new
-    init_order
-    empty_cart
+  expose_decorated :products, -> { find_products }
 
-    redirect_to users_orders_path
+  def new
+    @order = current_user.orders.new
+    @order.status = "Created"
+    @ordered_products = []
+
+    session[:cart].each do |id|
+      @ordered_products << OrderedProduct.new(
+        product_id: id, order_id: @order.id)
+    end
   end
 
   def index
+    @orders = current_user.orders.all
   end
 
   private
 
-  def init_order
-    order = current_user.orders.create status: "in_progress"
-    order.products << Product.where(id: session[:cart])
+  def find_products
+    Product.where id: session[:cart]
   end
+  # def init_order
+  #   order = current_user.orders.create status: "in_progress"
+  #   order.products << Product.where(id: session[:cart])
+  # end
 
-  def empty_cart
-    session[:cart] = []
-  end
+  # def empty_cart
+  #   session[:cart] = []
+  # end
 end
--- app/models/order.rb
+++ app/models/order.rb
@@ -1,9 +1,12 @@
 class Order < ApplicationRecord
   belongs_to :user
-
-  STATUSES = %w(in_progress sent done archived cancelled)
-  validates :status, inclusion: STATUSES
-
   has_many :ordered_products
   has_many :products, through: :ordered_products
+
+  STATUSES = %w(Created In_progress Sent Done Cancelled)
+  validates :status, inclusion: STATUSES
+
+  accepts_nested_attributes_for :ordered_products,
+                                allow_destroy: true,
+                                reject_if: :all_blank
 end
